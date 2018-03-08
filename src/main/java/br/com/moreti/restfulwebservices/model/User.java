ed6ce868dbabbf9258ee46ff089f8138c9f66e56
kmoreti <kleber.camargo@fatec.sp.gov.br>
2018-03-08 15:20:44 -0300

    Added Swagger, filtering, and security.

--- src/main/java/br/com/moreti/restfulwebservices/model/User.java
+++ src/main/java/br/com/moreti/restfulwebservices/model/User.java
@@ -1,14 +1,21 @@
 package br.com.moreti.restfulwebservices.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
+@ApiModel(description = "All details about the user.")
 public class User {
     private Integer id;
     @Size(min = 2, message = "Name should have at least 2 characters.")
+    @ApiModelProperty(notes = "Name should have at least 2 characters.")
     private String name;
+
     @Past
+    @ApiModelProperty(notes = "The birthdate should be in the past.")
     private LocalDate birthdate;
 
     protected User() {
